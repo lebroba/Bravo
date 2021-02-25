0b405ed5feba0cafdb6fcd31b920d3ed174ace9c
lebroba <blebron@yahoo.com>
2021-02-25 12:02:17 +0900

    Smart status board for CWC
    Nothing for now

---
+++ js/contacts.js
@@ -0,0 +1,327 @@
+{
+    "DESTROYERS": [{
+                  "id": "LUYANG III",
+                  "type":"DDG",
+                  "status": "CCOI",
+                  "RADAR": "HOTDOG",
+                  "WEAPONS": {
+                         "AIR WEZ": -117.125,
+                         "SURF WEZ": 31.125
+                  }
+           },
+           {
+                  "id": "LUYANG II",
+                  "type":"DDG",
+                  "status": "CCOI",
+                  "RADAR": "HOTDOG",
+                  "WEAPONS": {
+                         "AIR WEZ": -113.325,
+                         "SURF WEZ": 33.325
+                  }
+           },
+           {
+                  "id": "LUYANG I",
+                  "type":"DDG",
+                  "status": "CCOI",
+                  "RADAR": "HOTDOG",
+                  "WEAPONS": {
+                         "AIR WEZ": -113.325,
+                         "SURF WEZ": 33.325
+                  }
+           },
+           {
+                "id": "RENHAI",
+                "type":"DDG",
+                "status": "CCOI",
+                "RADAR": "CANDYCORN",
+                "WEAPONS": {
+                    "AIR WEZ": -113.325,
+                     "SURF WEZ": 33.325
+                  }
+           },
+           {
+                  "id": "SOVREMENNY",
+                  "type":"DDG",
+                  "status": "CCOI",
+                  "RADAR": "PRETZLE",
+                  "WEAPONS": {
+                         "AIR WEZ": -113.325,
+                         "SURF WEZ": 33.325
+                  }
+           },
+           {
+                  "id": "LUHAI",
+                  "type":"DDG",
+                  "status": "CCOI",
+                  "RADAR": "HOTDOG",
+                  "WEAPONS": {
+                         "AIR WEZ": -113.325,
+                         "SURF WEZ": 33.325
+                  }
+           },
+           {
+                  "id": "LUHU",
+                  "type":"DDG",
+                  "status": "CCOI",
+                  "RADAR": "HOTDOG",
+                  "WEAPONS": {
+                         "AIR WEZ": -113.325,
+                         "SURF WEZ": 33.325
+                  }
+           }
+    ],
+    "FRIGATES": [{
+        "id": "JIANGKAI II",
+        "type":"FFG",
+        "status": "CCOI",
+        "RADAR": "MILKDUDS",
+        "WEAPONS": {
+               "AIR WEZ": -117.125,
+               "SURF WEZ": 31.125
+        }
+ },
+ {
+        "id": "JIANGKAI I",
+        "type":"FFG",
+        "status": "CCOI",
+        "RADAR": "MILKDUDS",
+        "WEAPONS": {
+               "AIR WEZ": -113.325,
+               "SURF WEZ": 33.325
+        }
+ },
+ {
+        "id": "JIANGWEI II",
+        "type":"FFG",
+        "status": "CCOI",
+        "RADAR": "MILKDUDS",
+        "WEAPONS": {
+               "AIR WEZ": -113.325,
+               "SURF WEZ": 33.325
+        }
+ },
+ {
+      "id": "JIANGHU",
+      "type":"DD",
+      "status": "CCOI",
+      "RADAR": "MILKDUDS",
+      "WEAPONS": {
+          "AIR WEZ": -113.325,
+           "SURF WEZ": 33.325
+        }
+ },
+ {
+        "id": "JIANGDAO",
+        "type":"FFL",
+        "status": "CCOI",
+        "RADAR": "PAYDAY",
+        "WEAPONS": {
+               "AIR WEZ": -113.325,
+               "SURF WEZ": 33.325
+        }
+ },
+ {
+        "id": "HOUBEI",
+        "type":"PTG",
+        "status": "CCOI",
+        "RADAR": "SODAPOP",
+        "WEAPONS": {
+               "AIR WEZ": -113.325,
+               "SURF WEZ": 33.325
+        }
+ },
+ {
+        "id": "LUHU",
+        "type":"DDG",
+        "status": "CCOI",
+        "RADAR": "HOTDOG",
+        "WEAPONS": {
+               "AIR WEZ": -113.325,
+               "SURF WEZ": 33.325
+        }
+ }
+],
+    "SUBMARINES": [{
+        "id": "JIN",
+        "type":"SSBN",
+        "status": "CCOI",
+        "RADAR": "POPCORN",
+        "WEAPONS": {
+               "AIR WEZ": -117.125,
+               "SURF WEZ": 31.125,
+               "TDZ": 23.45
+        }
+ },
+    {
+        "id": "XIA",
+        "type":"SSBN",
+        "status": "CCOI",
+        "RADAR": "POPCORN",
+        "WEAPONS": {
+            "AIR WEZ": -117.125,
+            "SURF WEZ": 31.125,
+            "TDZ": 23.45
+    }
+ },
+ {
+    "id": "SHANG",
+    "type":"SSN",
+    "status": "CCOI",
+    "RADAR": "POPCORN",
+    "WEAPONS": {
+           "AIR WEZ": -117.125,
+           "SURF WEZ": 31.125,
+           "TDZ": 23.45
+    }
+ },
+ {
+    "id": "HAN",
+    "type":"SSN",
+    "status": "CCOI",
+    "RADAR": "POPCORN",
+    "WEAPONS": {
+           "AIR WEZ": -117.125,
+           "SURF WEZ": 31.125,
+           "TDZ": 23.45
+    }
+ },
+ {
+    "id": "YUAN",
+    "type":"SSN",
+    "status": "CCOI",
+    "RADAR": "POPCORN",
+    "WEAPONS": {
+           "AIR WEZ": -117.125,
+           "SURF WEZ": 31.125,
+           "TDZ": 23.45
+    }
+ },
+ {
+    "id": "SONG",
+    "type":"SSN",
+    "status": "CCOI",
+    "RADAR": "POPCORN",
+    "WEAPONS": {
+           "AIR WEZ": -117.125,
+           "SURF WEZ": 31.125,
+           "TDZ": 23.45
+    }
+ },
+ {
+    "id": "KILO",
+    "type":"SSN",
+    "status": "CCOI",
+    "RADAR": "POPCORN",
+    "WEAPONS": {
+           "AIR WEZ": -117.125,
+           "SURF WEZ": 31.125,
+           "TDZ": 23.45
+    }
+ },
+ {
+    "id": "MING",
+    "type":"SSN",
+    "status": "CCOI",
+    "RADAR": "POPCORN",
+    "WEAPONS": {
+           "AIR WEZ": -117.125,
+           "SURF WEZ": 31.125,
+           "TDZ": 23.45
+    }
+ }
+],
+"AMPHIB": [{
+    "id": "YUZHAO",
+    "type":"LPD",
+    "status": "CCOI",
+    "RADAR": "AVOCADO",
+    "WEAPONS": {
+           "AIR WEZ": -117.125,
+           "SURF WEZ": 31.125
+    }
+},
+{
+    "id": "YUTING III",
+    "type":"LSD",
+    "status": "CCOI",
+    "RADAR": "AVOCADO",
+    "WEAPONS": {
+           "AIR WEZ": -113.325,
+           "SURF WEZ": 33.325
+    }
+},
+{
+    "id": "YUTING II",
+    "type":"LSD",
+    "status": "CCOI",
+    "RADAR": "AVOCADO",
+    "WEAPONS": {
+           "AIR WEZ": -113.325,
+           "SURF WEZ": 33.325
+    }
+},
+{
+    "id": "YUTING",
+    "type":"LSD",
+    "status": "CCOI",
+    "RADAR": "AVOCADO",
+    "WEAPONS": {
+           "AIR WEZ": -113.325,
+           "SURF WEZ": 33.325
+    }
+},
+{
+    "id": "YUNSHU",
+    "type":"LSD",
+    "status": "CCOI",
+    "RADAR": "AVOCADO",
+    "WEAPONS": {
+           "AIR WEZ": -113.325,
+           "SURF WEZ": 33.325
+    }
+},
+{
+    "id": "YUBEI",
+    "type":"LSD",
+    "status": "CCOI",
+    "RADAR": "AVOCADO",
+    "WEAPONS": {
+           "AIR WEZ": -113.325,
+           "SURF WEZ": 33.325
+    }
+},
+{
+    "id": "YUHAI",
+    "type":"LSD",
+    "status": "CCOI",
+    "RADAR": "AVOCADO",
+    "WEAPONS": {
+           "AIR WEZ": -113.325,
+           "SURF WEZ": 33.325
+    }
+}
+],
+"AIRCRAFT CARRIER": [{
+    "id": "LIAONING",
+    "type":"CV",
+    "status": "CCOI",
+    "RADAR": "CHEESEBURGER",
+    "WEAPONS": {
+           "AIR WEZ": -117.125,
+           "SURF WEZ": 31.125,
+           "FEZ": 123.45
+    }
+},
+{
+    "id": "SHANDONG",
+    "type":"CV",
+    "status": "CCOI",
+    "RADAR": "CHEESEBURGER",
+    "WEAPONS": {
+           "AIR WEZ": -113.325,
+           "SURF WEZ": 33.325,
+           "FEZ": 123.45
+    }
+}
+]
+}
